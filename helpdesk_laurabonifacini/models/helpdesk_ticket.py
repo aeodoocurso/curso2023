e6f73d20935ad0a34dc5b42188f15dcdd707d3a3
LauraBoniBot <laura.bonifacini@adariagroup.com>
2023-04-25 14:43:54 +0000

    Tarea 25-04

--- helpdesk_laurabonifacini/models/helpdesk_ticket.py
+++ helpdesk_laurabonifacini/models/helpdesk_ticket.py
@@ -5,9 +5,12 @@ class HelpDeskTicket(models.Model):
     _description = 'Helpdesk Ticket'
 
     # Nombre
-    name = fields.Char(
-        required=True,
-        help="Título incidencia resumida."
+    name = fields.Char(required=True, help="Título incidencia resumida.")
+
+    # Secuencia
+    sequence = fields.Integer(
+        default=10,
+        help='Secuencia para'
     )
 
     # Descripción
@@ -21,12 +24,33 @@ class HelpDeskTicket(models.Model):
     date = fields.Date()
 
     # Fecha y hora limite
-    date_limit = fields.Datetime(string='Limit Date & Time')
+    date_limit = fields.Datetime(
+        string='Limit Date & Time')
 
     # Asignado (Verdadero o Falso)
-    assigned = fields.Boolean()
+    assigned = fields.Boolean(
+        readonly=True,
+    )
+    user_id = fields.Many2one(
+        comodel_name='res.users', 
+        string='Assigned to')
 
     # Acciones a realizar (Html)
     actions_todo = fields.Html()
+
+    # Añadir el campo estado
+    state = fields.Selection(
+        selection=[
+        ('new', 'New'),
+        ('assigned', 'Assigned'),
+        ('in_process', 'In process'),
+        ('pending', 'Pending'),
+        ('resolved', 'Resolved'),
+        ('canceled', 'Canceled'),
+        ],
+        default='new',
+    )
+
+    # 
 
     
