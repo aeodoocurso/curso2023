e5bc7bd4b118d1ee34d6b5d3dbf358858aeabfc3
Jordi Terol <jterol@ontinet.com>
2023-04-25 18:28:25 +0200

    TASK-01-02 JordiTerol

--- helpdesk_jorditerol/models/helpdesk_ticket.py
+++ helpdesk_jorditerol/models/helpdesk_ticket.py
@@ -8,10 +8,43 @@ class HelpdeskTicket(models.Model):
         required=True,
         help='Nombre de la incidencia'
     )
-    description = fields.Text()
+    sequence = fields.Char(
+        string='Sequence',
+        copy=False,
+    )
+
+    description = fields.Text(
+        default="""
+            Version a la que afecta:
+            Modulo:
+            Pasos para replicar:
+            Modulos personalizados:
+        """,
+    )
     date = fields.Date()
     date_limit = fields.Datetime(
-        string='Limit Date & Time'
+        string='Limit Date & Time',
     )
-    assigned = fields.Boolean()
+    assigned = fields.Boolean(
+        string='Assigned to',
+        readonly=True, 
+    )
+    
+    user_id = fields.Many2one(
+        comodel_name='res.users',
+        string='Assigned to',
+    )
+    
     actions_todo = fields.Html()
+
+    state = fields.Selection(
+        string='state',
+        selection=[
+            ('new', 'New'), 
+            ('assigned', 'Assigned'),
+            ('pending', 'Pending'),
+            ('resolved', 'Resolved'),
+            ('canceled', 'Canceled'),
+        ], 
+        default='new'
+    )
