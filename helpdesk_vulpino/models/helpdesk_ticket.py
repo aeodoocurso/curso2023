4bda53196db6ee428b5c31b310b4d02340b658e9
Vulpino <werewolftel@hotmail.com>
2023-04-25 15:38:09 +0000

    Taska 2 Dani Forga

--- helpdesk_vulpino/models/helpdesk_ticket.py
+++ helpdesk_vulpino/models/helpdesk_ticket.py
@@ -7,19 +7,26 @@ class HelpdeskTicket(models.Model):
 
     name = fields.Char(
         required=True)
+    user_id = fields.Many2one("res.users")
+    stage = fields.Selection([
+        ('new', 'New'),
+        ('asigned', 'Assigned'),
+        ('process', 'In process'),
+        ('pending', 'Pending'),
+        ('resolved', 'Resolved'),
+        ('cancel', 'Canceled')
+    ],
+    default="new")
     description = fields.Text(
         string="Description",
-        default="""
-        Afecta el salt de linia?
-
-        Aviam si funciona
-        """)
+        default="")
     date = fields.Date()
+    sequence = fields.Integer(default=10)
     limit_datetime = fields.Datetime(
-        string="Date & Time limit")
+        string="Date & Time limit",
+        help="Limit date")
     assign = fields.Boolean(
-        string="Is assign")
+        string="Is assign",
+        readonly=True)
     actions_todo = fields.Html(
         string="Actions to realize")
-
-    
