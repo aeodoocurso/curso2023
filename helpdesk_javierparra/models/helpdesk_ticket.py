b5ba69a88d464013cb10c0429be30aa4b7cd52e1
jparrareig <jparrareig@gmail.com>
2023-04-25 15:40:50 +0000

    tree and form views

--- helpdesk_javierparra/models/helpdesk_ticket.py
+++ helpdesk_javierparra/models/helpdesk_ticket.py
@@ -14,9 +14,9 @@ class HelpdeskTicket(models.Model):
     description = fields.Text(
         help="Escribe detalladamente la incidencia y como replicarla.",
         default="""Version a la que afecta:
-                    Modulo:
-                    Pasos para replicar:
-                    Modulos personalizados:
+    Modulo:
+    Pasos para replicar:
+    Modulos personalizados:
         """
     )
     #Fecha
@@ -24,7 +24,34 @@ class HelpdeskTicket(models.Model):
     #Fecha y hora limite
     date_limit = fields.Datetime(
         string='Limi Date & Time')    
-    #Asignado (Verdadero o Falso)
-    assigned = fields.Boolean()
+    #Asignado (Verdadero o Falso) solo lectura
+    assigned = fields.Boolean(
+        readonly=True,
+    )
     #Acciones a realizar (Html)
+
+    user_id = fields.Many2one(
+        comodel_name='res.users',
+        string='Assidned to')
+    
+    # Añadir el campo Estado [Nuevo, Asignado, En proceso, Pendiente, Resuelto, Cancelado], que por defecto sea Nuevo
+    state = fields.Selection(
+        selection=[
+            ('new', 'New'),
+            ('assigned', 'Assigned'),
+            ('in_process', 'In Process'),
+            ('pending', 'Pending'),
+            ('resolved', 'Resolved'),
+            ('canceled', 'Canceled'),
+        ],
+        default='new',
+    )
+
+    #Añádir campo sequence y hacer el widget handle.
+    sequence = fields.Integer(
+        default=10,
+        help="Secuencia para el orden de las incidencias",
+    )
+
+
     actions_todo = fields.Html()
