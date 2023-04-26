a475fe1c073627017170f7e562072ab5c178bf70
78Tonigil <78tonigil@gmail.com>
2023-04-26 16:39:48 +0000

    Helpdesk Tonigil

--- helpdesk_tonigil/__manifest__.py
+++ helpdesk_tonigil/__manifest__.py
@@ -15,6 +15,7 @@
     "data": [
         "security/helpdesk_security.xml",
         "security/ir.model.access.csv",
-        "views/helpdesk_ticket_views.xml"
+        "views/helpdesk_ticket_views.xml",
+        "views/helpdesk_ticket__tag_views.xml"
     ],
 }
