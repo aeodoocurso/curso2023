ca006d1e727a3fcee9de2973805a86845da559a9
Jordi Terol <jterol@ontinet.com>
2023-04-26 18:27:07 +0200

    Se sube la clase de hoy T01.03

---
+++ helpdesk_jorditerol/models/helpdesk_ticket_action.py
@@ -0,0 +1,29 @@
+from odoo import fields, models
+
+class HelpdeskTicketAction(models.Model):
+    _name = 'helpdesk.ticket.action'
+    _description = 'Helpdesk Ticket Action'
+
+    name = fields.Char(
+        string='Name',
+    )
+
+    state = fields.Selection(
+        string='state',
+        selection=[
+            ('todo', 'Para hacer'), 
+            ('done', 'Done'),
+        ], 
+        default='todo'
+    )
+
+    ticket_id = fields.Many2one(
+        'helpdesk.ticket',
+        string='Ticket',
+    )
+
+    def set_done(self):
+        self.write({'state': "done"})
+    
+    def set_todo(self):
+        self.write({'state': "todo"})
