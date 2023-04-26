7267f61e09af6c634a3784f0d61c8620bb953945
jterolc <jterolc@gmail.com>
2023-04-26 22:24:31 +0200

    TASK-01-03 JordiTerol

--- helpdesk_jorditerol/models/helpdesk_ticket.py
+++ helpdesk_jorditerol/models/helpdesk_ticket.py
@@ -38,13 +38,48 @@ class HelpdeskTicket(models.Model):
     actions_todo = fields.Html()
 
     state = fields.Selection(
-        string='state',
+        string='State',
         selection=[
-            ('new', 'New'), 
+            ('new',      'New'), 
             ('assigned', 'Assigned'),
-            ('pending', 'Pending'),
+            ('pending',  'Pending'),
             ('resolved', 'Resolved'),
             ('canceled', 'Canceled'),
         ], 
         default='new'
     )
+
+    def get_random_string(self, length):
+        import random
+        import string
+        letters = string.ascii_lowercase
+        result_str = ''.join(random.choice(letters) for i in range(length))
+        return result_str
+
+    def update_description(self):
+        for record in self:
+            record.description = self.get_random_string(12)
+        return True
+
+    def update_all_descriptions(self):
+        self.ensure_one()
+        all_tickets = self.env['helpdesk.ticket'].search([])
+        all_tickets.update_description()
+
+    tag_ids = fields.Many2many(
+        comodel_name='helpdesk.ticket.tag',
+        relation='helpdesk_ticket_tag_rel',
+        column1='ticket_id',
+        column2='tag_id',
+        string='Tags'
+    )
+
+    action_ids = fields.One2many(
+        comodel_name='helpdesk.ticket.action',
+        inverse_name='ticket_id',
+        string='Actions'
+    )
+
+    def set_actions_as_done(self):
+        self.ensure_one()
+        self.action_ids.set_done()
