4532019ca126112059c9ed3a59b76c177df8b58d
78Tonigil <78tonigil@gmail.com>
2023-05-02 17:36:39 +0000

    Helpdesk Tonigil

--- helpdesk_tonigil/models/helpdesk_ticket.py
+++ helpdesk_tonigil/models/helpdesk_ticket.py
@@ -85,3 +85,9 @@ class HelpdeskTicket(models.Model):
     def set_actions_as_done(self):
         self.ensure_one()
         self.action_ids.set_done()
+
+    color = fields.Integer('Color Index', default=0)
+
+    amount_time = fields.Float(
+        string='Amount of time'
+    )
