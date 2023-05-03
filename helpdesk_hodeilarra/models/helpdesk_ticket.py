a56628100dab8936408c00949998493e29028c13
Hodeilarra <hodei@fidenet.net>
2023-05-03 15:35:45 +0000

    Campos calculados

--- helpdesk_hodeilarra/models/helpdesk_ticket.py
+++ helpdesk_hodeilarra/models/helpdesk_ticket.py
@@ -1,4 +1,5 @@
-from odoo import fields, models
+from odoo import fields, models, api
+from odoo.exceptions import UserError
 
 class Helpdesk(models.Model):
     _name = 'helpdesk.ticket'
@@ -16,8 +17,7 @@ class Helpdesk(models.Model):
     #fecha y hora limite
     date_limit          = fields.Datetime(string="Limit Date & Time")
 
-    #Asignado
-    assigned            = fields.Boolean(readonly=True)
+    
 
     user_id             = fields.Many2one(comodel_name='res.users', string='Assigned to')
 
@@ -65,6 +65,58 @@ class Helpdesk(models.Model):
     def set_actions_as_done(self):
         self.ensure_one()
         self.action_ids.set_done()
-    
 
+    
+    #Asignado
+    assigned  = fields.Boolean(compute="_compute_assigned", search='_search_assigned', inverse='_inverse_assigned')
+
+    @api.depends('user_id')
+    def _compute_assigned(self):
+        for record in self:
+            record.assigned = bool(record.user_id)
+
+    def _search_assigned(self, operator, value):
+        if operator == '=' and value == True:
+            operator = '!='
+        else:
+            operator = '='
+        return [('user_id', operator, False)]
+
+    def _inverse_assigned(self):
+        for record in self:
+            if not record.assigned:
+                record.user_id = False
+            else:
+                record.user_id = self.env.user
+
+    user_name = fields.Char(related='user_id.name', string='User name', readonly=True)
+
+    ticket_count = fields.Integer(
+        compute='_compute_tickets_count',
+        string='Tickets count'
+    )
+
+    @api.depends('user_id')
+    def _compute_tickets_count(self):
+        ticket_obj = self.env['helpdesk.ticket']
+        for record in self:
+            tickets = ticket_obj.search([('user_id','=', record.user_id.id)])
+            record.ticket_count = len(tickets)
+
+    tag_name = fields.Char()
+
+    def create_tag(self):
+        self.ensure_one()
+        self.write({
+                'tag_ids': [(0,0,{'nombre':self.tag_name})]
+            })
+
+    def clear_tags(self):
+        self.ensure_one()
+        tag_ids = self.env['helpdesk.ticket.tag'].search([('nombre','=','otra')])
+        self.write({'tag_ids':[
+            (5,0,0),
+            (6,0,tag_ids.ids)
+        ]
+        })
 
