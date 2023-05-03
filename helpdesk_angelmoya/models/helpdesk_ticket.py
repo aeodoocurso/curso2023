db2af59ffd1a25222cf0ea474687934822e02bba
angelmoya <amoyapardo@gmail.com>
2023-05-03 14:15:20 +0000

    WIP 2-2

--- helpdesk_angelmoya/models/helpdesk_ticket.py
+++ helpdesk_angelmoya/models/helpdesk_ticket.py
@@ -1,4 +1,6 @@
-from odoo import fields, models
+from odoo import api, fields, models
+from odoo.exceptions import UserError
+
 
 class HelpdeskTicket(models.Model):
     _name = 'helpdesk.ticket'
@@ -78,3 +80,42 @@ class HelpdeskTicket(models.Model):
         self.action_ids.set_done()
     
     color = fields.Integer('Color Index', default=0)
+
+    amount_time = fields.Float(
+        string='Amount of time')
+    
+    person_id = fields.Many2one(
+        comodel_name='res.partner',
+        domain=[('is_company', '=', False)],)
+    
+    # Hacer que el campo assigned sea calculado,
+    # hacer que se pueda buscar con el atributo search y  
+    # hacer que se pueda modificar de forma que si lo marco se actualice el usuario con el usuario conectado y
+    # si lo desmarco se limpie el campo del usuario.
+    assigned = fields.Boolean(
+        compute='_compute_assigned',
+        search='_search_assigned',
+        inverse='_inverse_assigned',
+    )
+
+    @api.depends('user_id')
+    def _compute_assigned(self):
+        for record in self:
+            record.assigned = bool(record.user_id)
+    
+    def _search_assigned(self, operator, value):
+        if operator not in ('=', '!=') or not isinstance(value, bool):
+            raise UserError(_("Operation not supported"))
+        if operator == '=' and value == True:
+            operator = '!='
+        else:
+            operator = '='
+        return [('user_id', operator, False)]
+    
+    def _inverse_assigned(self):
+        for record in self:
+            if not record.assigned:
+                record.user_id = False
+            else:
+                record.user_id = self.env.user
+
