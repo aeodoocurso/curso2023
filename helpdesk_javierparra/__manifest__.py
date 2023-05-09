f541348fe59fbec1eb5748e1570b9aac4a17541f
jparrareig <jparrareig@gmail.com>
2023-05-09 20:48:53 +0000

    broken view Openrelated

--- helpdesk_javierparra/__manifest__.py
+++ helpdesk_javierparra/__manifest__.py
@@ -19,6 +19,7 @@
     "data": [
         "security/helpdesk_security.xml",
         "security/ir.model.access.csv",
+        "wizards/helpdesk_create_ticket_view.xml",
         "views/helpdesk_ticket_views.xml",
         "views/helpdesk_ticket_tag_views.xml",
         "views/helpdesk_ticket_action_views.xml",
---
+++ helpdesk_javierparra/wizards/helpdesk_create_ticket.py
@@ -0,0 +1,27 @@
+from odoo import models, api, fields, _
+
+
+class HelpdeskCreateTicket(models.TransientModel):
+    _name = 'helpdesk.create.ticket'
+    _description = 'Helpdesk Create Ticket'
+
+    tag_id = fields.Many2one('helpdesk.ticket.tag', string='Tag', required=True, default=lambda self: self.env.context.get('active_id'))
+    name = fields.Char(string='Name', required=True)
+    description = fields.Text(string='Description', required=True)
+
+
+    def create_ticket(self):
+        ticket = self.env['helpdesk.ticket'].create({
+            'name': self.name,
+            'description': self.description,
+            'tag_ids': [(4, self.tag_id.id)],
+        })
+
+        return {
+            'name': _('Ticket'),
+            'view_mode': 'form',
+            'res_model': 'helpdesk.ticket',
+            'res_id': ticket.id,
+            'type': 'ir.actions.act_window',
+        }
+
