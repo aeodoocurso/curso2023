c6b7912ca8e3e6426046d8605e2def5cd04ff193
jparrareig <jparrareig@gmail.com>
2023-05-09 09:05:58 +0000

    cron and more

--- helpdesk_javierparra/models/helpdesk_ticket.py
+++ helpdesk_javierparra/models/helpdesk_ticket.py
@@ -1,4 +1,5 @@
-from odoo import  fields, models, api, Command
+from odoo import  fields, models, api, Command, _
+from datetime import timedelta
 from odoo.exceptions import UserError
 
 class HelpdeskTicket(models.Model):
@@ -20,11 +21,32 @@ class HelpdeskTicket(models.Model):
     Modulos personalizados:
         """
     )
-    #Fecha
-    date = fields.Date()    
+    
+    #Fecha por defecto hoy
+    @api.model
+    def _get_default_date(self):
+        return fields.Date.today()
+
+    date = fields.Date(default=_get_default_date)    
+
+    
+
     #Fecha y hora limite
     date_limit = fields.Datetime(
         string='Limi Date & Time')    
+    
+
+    #Añadir un onchange para que al indicar la fecha ponga como fecha de vencimiento un día mas
+    #EL PROBLEMA ES QUE ESTO SOLO SE QUEDA EN LA VISTA
+    @api.onchange('date')
+    def _onchange_date(self):
+        if self.date:
+            self.date_limit = self.date + timedelta(days=1);
+        else:
+            self.date_limit = False
+    
+
+
     
     #Acciones a realizar (Html)
 
@@ -62,13 +84,20 @@ class HelpdeskTicket(models.Model):
 
     amount_time = fields.Float(string='Amount of time')
 
+    #añadir una restriccion que el campo no sea menor que 0
+    @api.constrains('amount_time')
+    def _check_amount_time(self):
+        for ticket in self:
+            if ticket.amount_time < 0:
+                raise UserError(_('Amount of time must be greater than 0'))
+
     tag_ids = fields.Many2many(
         comodel_name='helpdesk.ticket.tag',
         relation='helpdesk_ticket_tag_rel',
         column1='ticket_id',
         column2='tag_id',
         string='Tags')
-
+    
 
     #funcion que añada una accion
     #def add_action(self):
@@ -101,12 +130,22 @@ class HelpdeskTicket(models.Model):
         self.ensure_one()
         self.action_ids.set_done()
 
-    
+    @api.model
+    def _get_default_user_id(self):
+        print('get_default_user_id')
+        print('get_default_user_id')
+        print('get_default_user_id')
+        print(self.env.user)
+        print(self.env.user)
+        print(self.env.user)
+        return self.env.user
+
     #Asignado (Verdadero o Falso) solo lectura
     assigned = fields.Boolean(
         compute='_compute_assigned',
         search='_search_assigned',
         inverse='inverse_assigned',
+        default=_get_default_user_id,
     )
 
     #Hacer que el campo assigned sea calculado 
@@ -134,7 +173,7 @@ class HelpdeskTicket(models.Model):
             else:
                 ticket.user_id = self.env.user
 
-    tag_name = fields.Char(Readonly = True)
+    tag_name = fields.Char()
 
 
     #hacer un campo calculado que indique, dentro de un ticket, la cantidad de tiquets asociados al mismo ususario.
@@ -148,10 +187,37 @@ class HelpdeskTicket(models.Model):
             tickets = ticket.search([('user_id', '=', ticket.user_id.id)])
             ticket.ticket_count = len(tickets)
 
+#Modificar el botón de crear una etiqueta en el formulario de ticket para que abra una acción nueva, pasando por contexto el valor del nombre y la relación con el ticket.
+
+
+
+
 #    #crear un campo nombre de etiqueta, y hacer un botón que cree la nueva etiqueta con ese nombre y lo asocie al ticket.
     def create_tag(self):
+        
         self.ensure_one()
-        self.write({'tag_ids': [(0, 0, {'name': self.tag_name})]})
+        # self.write({'tag_ids': [(0,0,{'name': self.tag_name})]})
+        # self.write({'tag_ids': [Command.create({'name': self.tag_name})]})
+        # self.tag_ids = [Command.create({'name': self.tag_name})]
+        
+        #action = self.env["ir.actions.actions"]._for_xml_id("helpdesk_angelmoya.helpdesk_ticket_tag_action")
+        # <field name="name">Heldpesk Tickets Tags</field>
+        # <field name="type">ir.actions.act_window</field>
+        # <field name="res_model">helpdesk.ticket.tag</field>
+        action = {
+            'name': _('Heldpesk Tickets Tags'),
+            'type': 'ir.actions.act_window',
+            'res_model': 'helpdesk.ticket.tag',
+        }
+        action['context'] = {
+            'default_name': self.tag_name,
+            'default_ticket_id': self.id,
+        } 
+        #<field name="view_mode">form</field>
+        action['view_mode'] = 'form'
+        # action['binding_view_types'] = 'form'
+        action['target'] = 'new'
+        return action
 
 
     def clear_tags(self):
@@ -164,5 +230,16 @@ class HelpdeskTicket(models.Model):
             Command.set(tag_ids.ids)
         ]
 
+    def get_related_actions(self):
+        self.ensure_one()
+        action = self.env['ir.actions.action']._for_xml_id('helpdesk_javierparra.helpdesk_ticket_action_action')
+        action['domain'] = [('ticket_id', '=', self.id)]
+        action ['context'] = {'default_ticket_id': self.id}
+        return action        
 
+
+    def set_assigned(self):
+        self.ensure_one()
+        self.state = 'assigned'
+        self.user_id = self.env.user
 
--- helpdesk_javierparra/models/helpdesk_ticket_tag.py
+++ helpdesk_javierparra/models/helpdesk_ticket_tag.py
@@ -1,4 +1,4 @@
-from odoo import  fields, models
+from odoo import  fields, models, api
 
 class HelpdeskTicketTag(models.Model):
     _name = 'helpdesk.ticket.tag'
@@ -17,3 +17,10 @@ class HelpdeskTicketTag(models.Model):
         column1='tag_id',
         column2='ticket_id',
         string='Tickets')
+    
+    @api.model
+    def _clean_unused_tags_cron(self):
+        all_tags = self.env['helpdesk.ticket.tag'].search([])
+        for tag in all_tags:
+            if not tag.ticket_ids:
+                tag.unlink()
